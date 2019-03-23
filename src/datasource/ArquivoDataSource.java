0d8a5713eff96739311d2226cb4b42b9b74168ed
fborinelli <fabio_aki@hotmail.com>
2019-03-23 09:50:33 -0300

    GitIgnore

---
+++ src/datasource/ArquivoDataSource.java
@@ -0,0 +1,5 @@
+package datasource;
+
+public class ArquivoDataSource implements IDataSource{
+
+}
---
+++ src/datasource/IDataSource.java
@@ -0,0 +1,8 @@
+package datasource;
+
+public interface IDataSource {
+	public void listar();
+	public void incluir();
+	public void excluir();
+	public void alterar();
+}
